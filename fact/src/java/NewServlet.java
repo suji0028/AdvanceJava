2fec7238f519e5e830cba7b7202b840b08d7e925
Sachin Gardhariya <154439826+suji0028@users.noreply.github.com>
2025-03-26 20:51:24 +0530

    Add files via upload

---
+++ fact/src/java/NewServlet.java
@@ -0,0 +1,70 @@
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class NewServlet extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            
+            String rollno = request.getParameter("roll");
+            String name = request.getParameter("name");
+            int sub1 = Integer.parseInt(request.getParameter("sub1"));
+            int sub2 = Integer.parseInt(request.getParameter("sub2"));
+            int sub3 = Integer.parseInt(request.getParameter("sub3"));
+            
+            int total = sub1 + sub2 + sub3;
+            double average = total / 3.0;
+            
+            out.println("<br>Name is:"+name);
+            out.println("<br>Roll No is:"+rollno);
+            out.println("<br>Total is:"+total);
+            out.println("<br>Average is:"+average);
+            
+        }
+    }
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+}
